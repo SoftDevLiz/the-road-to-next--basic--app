1ccf5fda87264186bf17b817a6946a2d81cf0187
Liezl Mouton <liz.mouton1@gmail.com>
2025-04-23 18:13:09 +0200

    Add static and dynamic routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import Link from "next/link";
+
 const HomePage = () => {
-  return <div className="text-3xl p-4">Home</div>;
+  return (
+    <div className="text-3xl p-4">
+      <h2>Home</h2>
+      <Link href="/tickets" className="underline">View tickets</Link>
+    </div>
+  );
 };
 
 export default HomePage;
--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,3 +1,4 @@
+import { initialTickets } from "@/data";
 
 type TicketPageProps = {
     params: Promise<{
@@ -8,7 +9,18 @@ type TicketPageProps = {
 const TicketPage = async ({ params }: TicketPageProps) => {
     const { ticketId } = await params;
 
-    return <div className="text-3xl p-4">Ticket number {ticketId}</div>;
+    const ticket = initialTickets.find((ticket) => ticket.id === ticketId);
+
+    if (!ticket) {
+        return <div className="text-3xl p-4">Ticket not found</div>
+    }
+
+    return (
+        <div>
+            <h2 className="text-3xl p-4">{ticket.title}</h2>
+            <h3 className="p-4">{ticket.content}</h3>
+        </div>
+    );
 };
 
 export default TicketPage;
