b0b3f3663eb82e20c9fcbd05dcb76abc1d330120
Liezl Mouton <liz.mouton1@gmail.com>
2025-04-24 15:45:17 +0200

    Add typescript-eslint to lint ts files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { FlatCompat } from "@eslint/eslintrc";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { dirname } from "path";
+import tseslint from "typescript-eslint";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,8 +11,29 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+// Load your tsconfig path
+const tsconfigPath = "./tsconfig.json";
+
+// Define the ESLint config
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // TypeScript-ESLint with type-aware rules
+  ...tseslint.config(
+    {
+      files: ["**/*.ts", "**/*.tsx"],
+      languageOptions: {
+        parserOptions: {
+          project: tsconfigPath,
+          tsconfigRootDir: __dirname,
+        },
+      },
+    },
+    tseslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked
+  ),
+
+  // Simple Import Sort
   {
     plugins: {
       "simple-import-sort": simpleImportSort,
