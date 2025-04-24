f5d22188f8a55b546d2ece277fc0481c124fd312
Liezl Mouton <liz.mouton1@gmail.com>
2025-04-24 18:04:58 +0200

    Animate tickets on tickets page

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import clsx from 'clsx';
 import { initialTickets } from '@/data'
 import { ticketPath } from '@/paths';
 
@@ -16,12 +17,12 @@ const TicketsPage = () => {
                 <h2 className="text-3xl font-bold tracking-tight">Tickets page</h2>
                 <p className="text-sm text-muted-foreground">All your tickets in one place</p>
             </div>
-            <div className='flex-1 flex flex-col items-center gap-y-4'>
+            <div className='flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top'>
                 {initialTickets.map((ticket) => (
                     <div key={ticket.id} className='w-full max-w-[420px] p-4 border border-slate-100 rounded'>
                         <h3>{TICKET_ICONS[ticket.status]}</h3>
                         <h3 className="text-lg truncate font-semibold">{ticket.title}</h3>
-                        <p className='text-sm text-slate-500 truncate'>{ticket.content}</p>
+                        <p className={clsx('text-sm text-slate-500 truncate', {"line-through": ticket.status === "DONE"})}>{ticket.content}</p>
                         <Link href={ticketPath(ticket.id)} className='underline text-lg'>View</Link>
                     </div>
                 ))}
