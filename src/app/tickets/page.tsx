3e687f425d868758a60da94135ff9d3b8ee6950f
Liezl Mouton <liz.mouton1@gmail.com>
2025-04-27 12:11:49 +0200

    Create custom heading component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,7 +1,7 @@
 import { LucideCircleCheck,LucideEye, LucideEyeClosed } from 'lucide-react';
 import Link from 'next/link';
+import { Heading } from '@/components/heading'
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card' 
-import { Separator } from '@/components/ui/separator'
 import { initialTickets } from '@/data'
 import { ticketPath } from '@/paths';
 
@@ -15,31 +15,27 @@ const TICKET_ICONS = {
 const TicketsPage = () => {
     return (
         <div className='flex-1 flex flex-col gap-y-8'>
-            <div>
-                <h2 className="text-3xl font-bold tracking-tight">Tickets page</h2>
-                <p className="text-sm text-muted-foreground">All your tickets in one place</p>
-            </div>
-            <Separator />
-            <div className='flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top'>
-                {/* Loops over ticket data and renders individual tickets*/}
-                {initialTickets.map((ticket) => (
-                    <Card key={ticket.id} className='w-full max-w-[420px]'>
-                        {/* [ticket.status] is mapped against TICKET_ICONS because the initialTickets holds the actual status of the ticket and TICKET_ICONS holds the related SVG */}
-                        <CardHeader>
-                            <CardTitle className='flex gap-2 items-center'>
-                                <span>{TICKET_ICONS[ticket.status]}</span>
-                                <span className='truncate'>{ticket.title}</span>
-                            </CardTitle>
-                        </CardHeader>
-                        <CardContent>
-                            <span className='line-clamp-3 whitespace-break-spaces'>{ticket.content}</span>
-                        </CardContent>
-                        <CardFooter>
-                            <Link href={ticketPath(ticket.id)} className='underline text-lg'>View</Link>
-                        </CardFooter>
-                    </Card>
-                ))}
-            </div>
+            <Heading title='Tickets' description='All your tickets in one place'/>
+                <div className='flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top'>
+                    {/* Loops over ticket data and renders individual tickets*/}
+                    {initialTickets.map((ticket) => (
+                        <Card key={ticket.id} className='w-full max-w-[420px]'>
+                            {/* [ticket.status] is mapped against TICKET_ICONS because the initialTickets holds the actual status of the ticket and TICKET_ICONS holds the related SVG */}
+                            <CardHeader>
+                                <CardTitle className='flex gap-2 items-center'>
+                                    <span>{TICKET_ICONS[ticket.status]}</span>
+                                    <span className='truncate'>{ticket.title}</span>
+                                </CardTitle>
+                            </CardHeader>
+                            <CardContent>
+                                <span className='line-clamp-3 whitespace-break-spaces'>{ticket.content}</span>
+                            </CardContent>
+                            <CardFooter>
+                                <Link href={ticketPath(ticket.id)} className='underline text-lg'>View</Link>
+                            </CardFooter>
+                        </Card>
+                    ))}
+                </div>
         </div>
     )
 };
