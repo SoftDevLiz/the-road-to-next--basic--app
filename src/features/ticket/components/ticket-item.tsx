e43ec9a0dd322592ec0c60819749b64d9cb233e8
Liezl Mouton <liz.mouton1@gmail.com>
2025-04-28 18:29:17 +0200

    Fix small card style issue

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -10,21 +10,21 @@ type TicketItemProps = {
 
 const TicketItem = ({ ticket }: TicketItemProps) => {
     return (
-        <Card className='w-full max-w-[420px]'>
-            {/* [ticket.status] is mapped against TICKET_ICONS because the initialTickets holds the actual status of the ticket and TICKET_ICONS holds the related SVG */}
-            <CardHeader>
-                <CardTitle className='flex gap-2 items-center'>
-                    <span>{TICKET_ICONS[ticket.status]}</span>
-                    <span className='truncate'>{ticket.title}</span>
-                </CardTitle>
-            </CardHeader>
-            <CardContent>
-                <span className='line-clamp-3 whitespace-break-spaces'>{ticket.content}</span>
-            </CardContent>
-            <CardFooter>
-                <Link href={ticketPath(ticket.id)} className='underline text-lg'>View</Link>
-            </CardFooter>
-        </Card>
+            <Card className='w-full max-w-[420px]'>
+                {/* [ticket.status] is mapped against TICKET_ICONS because the initialTickets holds the actual status of the ticket and TICKET_ICONS holds the related SVG */}
+                <CardHeader>
+                    <CardTitle className='flex gap-2 items-center'>
+                        <span>{TICKET_ICONS[ticket.status]}</span>
+                        <span className='truncate'>{ticket.title}</span>
+                    </CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <span className='line-clamp-3 whitespace-break-spaces'>{ticket.content}</span>
+                </CardContent>
+                <CardFooter>
+                    <Link href={ticketPath(ticket.id)} className='underline text-lg'>View</Link>
+                </CardFooter>
+            </Card>  
     )
 }
 
