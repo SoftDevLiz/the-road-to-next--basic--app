b107afddef9ae21bbdf3528755b4aa00c0d4a3bc
Liezl Mouton <liz.mouton1@gmail.com>
2025-05-13 18:55:30 +0200

    Add comment for Suspense component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -8,6 +8,8 @@ const TicketsPage = () => {
     return (
         <div className='flex-1 flex flex-col gap-y-8'>
             <Heading title='Tickets' description='All your tickets in one place'/>
+            
+            {/* Suspense is a React component. Hover over it to learn more. */}
             <Suspense fallback={<Spinner />}>
                 <TicketList />
             </Suspense>
