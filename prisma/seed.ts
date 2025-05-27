c0d4b7c78b7fbfbff57452eae88823023e19ef8c
Liezl Mouton <liz.mouton1@gmail.com>
2025-05-27 16:54:14 +0200

    Start database seed file with 3 tickets

---
+++ prisma/seed.ts
@@ -0,0 +1,20 @@
+const tickets = [
+    {
+        id: "1",
+        title: "Ticket 1",
+        content: "First ticket from the database",
+        status: "DONE" as const,
+    },
+    {
+        id: "2",
+        title: "Ticket 2",
+        content: "Second ticket from the database",
+        status: "OPEN" as const,
+    },
+    {
+        id: "3",
+        title: "Ticket 3",
+        content: "Third ticket from the database",
+        status: "IN_PROGRESS" as const,
+    },
+]
