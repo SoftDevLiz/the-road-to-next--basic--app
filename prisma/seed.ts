d4bb50c7d7c77bb75a37a582da7dbc58db5ea02e
Liezl Mouton <liz.mouton1@gmail.com>
2025-05-27 17:25:00 +0200

    Removed custom ids in seed file so prisma can generate ids for us

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,21 +4,18 @@ const prisma = new PrismaClient();
 
 const tickets = [
     {
-        id: "1",
         title: "Ticket 1",
         content: "First ticket from the database",
         status: "DONE" as const,
         updatedAt: new Date(),
     },
     {
-        id: "2",
         title: "Ticket 2",
         content: "Second ticket from the database",
         status: "OPEN" as const,
         updatedAt: new Date(),
     },
     {
-        id: "3",
         title: "Ticket 3",
         content: "Third ticket from the database",
         status: "IN_PROGRESS" as const,
