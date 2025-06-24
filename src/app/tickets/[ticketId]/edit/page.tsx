bdf77282916e8b71a23efb3544589286890fb69a
Liezl Mouton <liz.mouton1@gmail.com>
2025-06-24 21:58:43 +0200

    Create edit ticket page

--- src/app/tickets/[ticketId]/edit/page.tsx
+++ src/app/tickets/[ticketId]/edit/page.tsx
@@ -1,10 +1,12 @@
-import { Heading } from "@/components/heading";
+import CardCompact from "@/components/card-compact";
+import TicketUpdateForm from "@/features/ticket/components/ticket-update-form";
+
 
 const EditTicketPage = () => {
 
     return (
-        <div>
-            <Heading title="Edit Ticket" description="Edit this ticket" />
+        <div className="flex-1 flex flex-col justify-center items-center">
+            <CardCompact title="Edit Ticket" description="Edit this ticket" content={<TicketUpdateForm />} className="w-full max-w-[420px] animate-fade-from-top"/>
         </div>
     )
     
