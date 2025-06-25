74defe365bfb5b800007cb15e698c21121eeed91
Liezl Mouton <liz.mouton1@gmail.com>
2025-06-25 17:55:41 +0200

    Add in functionality to the edit page

--- src/app/tickets/[ticketId]/edit/page.tsx
+++ src/app/tickets/[ticketId]/edit/page.tsx
@@ -1,12 +1,25 @@
+import { notFound } from "next/navigation";
 import CardCompact from "@/components/card-compact";
 import TicketUpdateForm from "@/features/ticket/components/ticket-update-form";
+import { getTicket } from "@/features/ticket/queries/get-ticket";
 
+type TicketEditPageProps = {
+    params: Promise<{ ticketId: string }>
+}
 
-const EditTicketPage = () => {
+const EditTicketPage = async ({ params }: TicketEditPageProps) => {
+
+    // fetch entire ticket to pass to the update form
+    const { ticketId } = await params;
+    const ticket = await getTicket(ticketId);
+
+    if (!ticket) {
+        notFound();
+    }
 
     return (
         <div className="flex-1 flex flex-col justify-center items-center">
-            <CardCompact title="Edit Ticket" description="Edit this ticket" content={<TicketUpdateForm />} className="w-full max-w-[420px] animate-fade-from-top"/>
+            <CardCompact title="Edit Ticket" description="Update an existing ticket" content={<TicketUpdateForm ticket={ticket} />} className="w-full max-w-[420px] animate-fade-from-top"/>
         </div>
     )
     
