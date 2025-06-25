91eb002ebde24af3e3c5b3b8569d80838cd75287
Liezl Mouton <liz.mouton1@gmail.com>
2025-06-25 18:13:56 +0200

    small change

--- src/features/ticket/actions/update-ticket.tsx
+++ src/features/ticket/actions/update-ticket.tsx
@@ -6,11 +6,10 @@ import { prisma } from "@/lib/prisma";
 import { ticketsPath } from "@/paths";
 
 /** UpdateTicket is a server action. Takes ticket id and other details from the update form to update the ticket with. */
-const updateTicket = async (formData: FormData) => {
+const updateTicket = async (id: string, formData: FormData) => {
 
     // Succintly extracts the title and content from the form and packs it nicely into a data object for use in in the prisma update call.
     const data = {
-        id: formData.get("id"),
         title: formData.get("title"),
         content: formData.get("content"),
     };
